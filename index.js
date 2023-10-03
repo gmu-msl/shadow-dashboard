0b52c630ca50997db277906e9cc65366e3d4710e
Pavan Kumar D <kumarpavan1309@gmail.com>
2023-10-03 15:26:04 -0400

    enable debug logs

--- index.js
+++ index.js
@@ -11,16 +11,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // setup cors
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN || '*', // By default allow all origins
-}));
+  })
+);
 
 // setup template engine
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 routes(app);
