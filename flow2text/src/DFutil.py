4006fc8117b005ba9a7b0f06d6074efe2791587c
Taylor Henderson <tph5595@verizon.net>
2024-04-24 15:52:03 -0400

    Update DFutil.py

--- flow2text/src/DFutil.py
+++ flow2text/src/DFutil.py
@@ -6,6 +6,7 @@ def df_to_ts(df):
     if not isinstance(df.index, pd.DatetimeIndex):
         print(df)
     # tmp = df.resample('100ms').sum(numeric_only=True).infer_objects()
+    # (1000 second sliding window) / (300 times speed up) = 3.333s = 3333ms
     tmp = df.resample('3333ms').sum(numeric_only=True).infer_objects()
     tmp = tmp.reset_index()
     return tmp
